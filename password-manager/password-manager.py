915e52dc00a79c99a69a710a6f79fea7d95d1cb0
Mateus Sartori <98856859+Mathiuw@users.noreply.github.com>
2026-07-07 19:24:04 -0300

    Json data operations

--- password-manager/password-manager.py
+++ password-manager/password-manager.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import password_generator
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -23,7 +24,7 @@ def save():
         return
 
     is_ok = messagebox.askokcancel(title="Password added", message=f'''Confirm the details entered:
-    
+
 Website: {website}
 
 Username: {user}
@@ -33,11 +34,46 @@ Password: {password}
 Is it ok to save?
     ''')
 
-    if is_ok:
-        with open("data.txt", "a") as data_file:
-            data_file.write(f"{website} | {user} | {password}\n")
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
+    if not is_ok: return
+
+    new_data = {
+        website: {
+            "username": user,
+            "password": password
+        }
+    }
+
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        with open("data.json", "w") as data_file:
+            json.dump(new_data, data_file, indent=4)
+    else:
+        data.update(new_data)
+        with open("data.json", "w") as data_file:
+            json.dump(data, data_file, indent=4)
+    finally:
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
+# ---------------------------- PASSWORD SEARCH ------------------------------- #
+
+def search_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showwarning(title="File not found", message="There's no saved passwords yet!")
+    else:
+        if website in data:
+            user = data[website]["username"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"User: {user} \n Password: {password}")
+        else:
+            messagebox.showwarning(title="Invalid Password", message="Password was not found!")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -61,8 +97,8 @@ password_label = Label(text="Password")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row= 1, column= 1, columnspan= 2)
+website_entry = Entry(width=21)
+website_entry.grid(row= 1, column= 1)
 website_entry.focus()
 
 user_entry = Entry(width= 35)
@@ -73,6 +109,9 @@ password_entry = Entry(width= 21)
 password_entry.grid(row= 3, column= 1)
 
 # Buttons
+search_password_button = Button(width= 14, text="Search", command=search_password)
+search_password_button.grid(row=1, column= 2)
+
 generate_password_button = Button(width= 14, text="Generate Password", command=create_password)
 generate_password_button.grid(row=3, column= 2)
 
